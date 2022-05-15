215b4cf141555d99477aa8116b7d24f3e7f6c33e
xuvea <90873349+xuvea@users.noreply.github.com>
2022-05-14 22:55:32 -0700

    function setup
    setting up functions

--- main.py
+++ main.py
@@ -2,10 +2,21 @@
 
 import numpy, tkinter, matlab, ast, inspect
 
-#call in the code file
 
-#parse the file
+#some kind of GUI for the user to use
+
+#call in the code file and parse the file, by reading the function types , variable types, save into
+# an array maybe
+def parseMe():
+  pass
 
 #create an array of notes?
-
 #apply effects to sections of array?
+def makeMusic():
+  pass
+
+def main():
+  pass
+
+if __name__ == "__main__":
+  main()
