ce024b9ec5d804ddedf545987fe86d5a211e104d
xuvea <90873349+xuvea@users.noreply.github.com>
2022-05-14 22:46:39 -0700

    notes
    writing out notes

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #start of something wonderful
+
+import numpy, tkinter, matlab, ast, inspect
+
+#call in the code file
+
+#parse the file
+
+#create an array of notes?
+
+#apply effects to sections of array?
